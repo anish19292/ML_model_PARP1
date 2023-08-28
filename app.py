f9f34b431f1ef90593b746c255131290a782ed52
Anish Gomatam <119532203+anish19292@users.noreply.github.com>
2023-08-28 12:38:05 +0530

    Update app.py

--- app.py
+++ app.py
@@ -126,10 +126,11 @@ with st.expander('Show scaled descriptors as used in trained model'):
 st.subheader('🤖 Predictions')
 pred_array = loaded_classifier.predict(scaled_query_desc)
 pred = int(pred_array[0])  # Extract the single element from the array
-    if pred == 0:
-        st.error('This compound is expected to be PARP-1 inactive.')
-    if pred == 1:
-        st.success('This compound is expected to be PARP-1 active.')
+
+if pred == 0:
+    st.error('This compound is expected to be PARP-1 inactive.')
+if pred == 1:
+    st.success('This compound is expected to be PARP-1 active.')
 
 with tab2:
     st.header('Dataset')
